c00f16e59f87d5f898fa72d3b880552a6c0823e3
An Ho <41894751+hothaian@users.noreply.github.com>
2024-03-30 10:43:13 -0700

    create mock sample to populate on Apollo server

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,22 +1,32 @@
 const gql = require("graphql-tag");
 
 const typeDefs = gql`
-  type Querry {
-    "give all the non-null tracks array"
-    trackForHome: [Track!]!
+  type Query {
+    "Query to get tracks array for the homepage grid"
+    tracksForHome: [Track!]!
   }
+
+  "A track is a group of Modules that teaches about a specific topic"
   type Track {
     id: ID!
+    "The track's title"
     title: String!
+    "The track's main Author"
     author: Author!
-    thumnail: String
+    "The track's illustration to display in track card or track page detail"
+    thumbnail: String
+    "The track's approximate length to complete, in minutes"
     length: Int
+    "The number of modules this track contains"
     modulesCount: Int
   }
 
+  "Author of a complete Track or a Module"
   type Author {
     id: ID!
+    "Author's first and last name"
     name: String!
+    "Author's profile picture"
     photo: String
   }
 `;
